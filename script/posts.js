09a455e5ded7d557ab77ec2c5dc3706dc6c82e72
KityLinn <87649871+KityLinn@users.noreply.github.com>
2023-10-11 22:00:13 +0200

    10 posts fetch and render function

--- script/posts.js
+++ script/posts.js
@@ -20,5 +20,20 @@ async function getPosts(url) {
 	});
 	const data = await res.json();
 	console.log(data);
+	var main = document.querySelector("#posts");
+	main.innerHTML = "";
+	var html = ""
+	for (var i = 0; i < data.length; i++) {
+		html += createPost(data[i]); 
+	  }
+	  main.innerHTML = html;
 }
+function createPost(data) {
+	return `<div class="post">
+			  <h2>${data.title}</h2>
+			  <p>${data.body}</p>
+			  <button onclick="showDetails(${data.id})">Details</button>
+			</div>`;
+  }
+
 getPosts(urls.posts)

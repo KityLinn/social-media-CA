8e78ad42ff4704a4ddc21e5e71042f923ab55103
KityLinn <87649871+KityLinn@users.noreply.github.com>
2023-10-11 16:44:00 +0200

    added email in login

--- script/login.js
+++ script/login.js
@@ -11,12 +11,14 @@ const urls = {
 const logIn = document.querySelector("#login");
 const logUsername = document.querySelector("#login-name");
 const logPassword = document.querySelector("#login-password");
+const logemail = document.querySelector("#login-email")
 
 logIn.addEventListener("click", (e) => {
     e.preventDefault()
     const userLogin = {
         username: logUsername.value,
         password: logPassword.value,
+        email: logemail.value,
     };
 
     loginFunc(urls.login, userLogin)
@@ -37,6 +39,7 @@ const loginFunc = async (loginURL, userData) => {
 	console.log(data);
 	localStorage.setItem("token", data.accessToken);
     window.location.href = "./posts.html"
+
 }
 
 

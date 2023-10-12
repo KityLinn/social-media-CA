4a681b1e54b3da8f7c334713c5d6c2359ee834fe
KityLinn <87649871+KityLinn@users.noreply.github.com>
2023-10-12 21:38:14 +0200

    html for post creation, post creation function outline

--- script/createpost.js
+++ script/createpost.js
@@ -1,2 +1,32 @@
 import { urls } from "./module.mjs";
 
+const postSubmit = document.querySelector("#post-submit");
+const postTitle = document.querySelector("#post-title");
+const postContent = document.querySelector("#post-content");
+
+postSubmit.addEventListener("click", (e) => {
+    e.preventDefault()
+    const userLogin = {
+        title: postTitle.value,
+        body: postContent.value,
+    };
+
+    createPost(urls.login, userLogin)
+});
+
+
+
+const createPost = async (createURL, postData) => {
+    const res = await fetch (createURL, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+    });
+    const data = await res.json();
+	console.log(data);
+	localStorage.setItem("token", data.accessToken);
+    window.location.href = "./posts.html"
+
+}

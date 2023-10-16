37016d862df93be6773736f56069b49bbc37dec5
KityLinn <87649871+KityLinn@users.noreply.github.com>
2023-10-16 16:30:04 +0200

    created login validation

--- script/register.js
+++ script/register.js
@@ -4,8 +4,11 @@ import { urls } from "./module.mjs";
 
 const register = document.querySelector("#Signup");
 const regUsername = document.querySelector("#singup-name");
-const regEmail = document.querySelector("signup-email");
+const regEmail = document.querySelector("#signup-email");
 const regPassword = document.querySelector("#signup-password");
+const errorsDiv = document.querySelector("#errors-div");
+
+
 
 register.addEventListener("click", (e) => {
     e.preventDefault()
@@ -29,8 +32,19 @@ const registerFunc = async (registerURL, userData) => {
         body: JSON.stringify(userData),
     });
     const data = await res.json();
-	console.log(data);
-    regiLogin(urls.login, userData)
+    if (data.errors) {
+        errorsDiv.innerHTML = ""
+        var errorContainer = ""
+        for (var i = 0; i < data.errors.length; i++) {
+            console.log(data.errors[i])
+            errorContainer += createError(data.errors[i])
+          }
+          errorsDiv.innerHTML = errorContainer;
+    }
+    else {
+        regiLogin(urls.login, userData);
+
+    }
     
 }
 
@@ -43,12 +57,15 @@ const regiLogin = async (url, loginData) => {
         body: JSON.stringify(loginData),
     });
     const data = await res.json();
+    
 	console.log(data);
 	localStorage.setItem("token", data.accessToken);
     window.location.href = "./posts.html"
 }
 
-
+const createError = (data) => {
+    return `<p id="error">${data.message}</p>`
+}
 /* test user
 name: testKit2
 email: testKit2@noroff.no

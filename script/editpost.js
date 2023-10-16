87560980fc401bb32be9baae32da13b915daf096
KityLinn <87649871+KityLinn@users.noreply.github.com>
2023-10-16 14:44:38 +0200

    edit posts styling and object destructuring

--- script/editpost.js
+++ script/editpost.js
@@ -16,26 +16,27 @@ const getEdit = async (url) => {
 	});
 	const data = await res.json();
 	console.log(data);
+  var {title, body} = data;
 	var main = document.querySelector("#posts");
 	main.innerHTML = "";
 	  main.innerHTML += `
-      <form id="posts">
-      Create Post
-      <div class="form-outline">
-        <input type="text" id="post-title" name="title" value="${data.title}"/>
-        <label class="form-label" for="post-title">Your post title</label>
-      </div>
-      <div class="form-outline">
-        <textarea name="content" id="post-content" cols="30" rows="10">${data.body}</textarea>
-        <label class="form-label" for="post-content">Your post content</label>
-      </div>
-      <div>
-        <button type="submit" id="post-edit">Submit your edited post</button>
-      </div>
-    </form> `;
+  <div class="create-div">
+    <label class="form-label" for="post-title">Your post title</label>
+    <input type="text" id="post-title" name="title" value ="${title}"/>
+  </div>
+
+  <div class="create-div">
+    <label class="form-label" for="post-content">Your post content</label>
+    <textarea name="content" id="post-content" cols="30" rows="10">${body}</textarea>
+  </div>
+
+  <div>
+    <button type="submit" id="post-edit">Submit your post</button>
+  </div> `;
 const postEdit = document.querySelector("#post-edit");
 const postTitle = document.querySelector("#post-title");
 const postContent = document.querySelector("#post-content");
+
 
 postEdit.addEventListener("click", (e) => {
     e.preventDefault()

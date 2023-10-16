9241f95ff2247541db70ebfd3623bd4923879e8b
KityLinn <87649871+KityLinn@users.noreply.github.com>
2023-10-16 12:11:42 +0200

    sign up page styling

--- script/register.js
+++ script/register.js
@@ -2,10 +2,10 @@ import { urls } from "./module.mjs";
 
 
 
-const register = document.querySelector("#Sign-up");
-const regUsername = document.querySelector("#name-1");
-const regEmail = document.querySelector("#email-1");
-const regPassword = document.querySelector("#password-1");
+const register = document.querySelector("#Signup");
+const regUsername = document.querySelector("#singup-name");
+const regEmail = document.querySelector("signup-email");
+const regPassword = document.querySelector("#signup-password");
 
 register.addEventListener("click", (e) => {
     e.preventDefault()

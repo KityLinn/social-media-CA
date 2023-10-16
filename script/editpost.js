27c7c6a7410cf577f856d8a3b01b251c6f76457d
KityLinn <87649871+KityLinn@users.noreply.github.com>
2023-10-16 15:46:15 +0200

    error for editing post

--- script/editpost.js
+++ script/editpost.js
@@ -30,8 +30,9 @@ const getEdit = async (url) => {
     <textarea name="content" id="post-content" cols="30" rows="10">${body}</textarea>
   </div>
 
-  <div>
+  <div class="create-div">
     <button type="submit" id="post-edit">Submit your post</button>
+    <p id="error"></p>
   </div> `;
 const postEdit = document.querySelector("#post-edit");
 const postTitle = document.querySelector("#post-title");
@@ -61,7 +62,14 @@ const editPost = async (editURL, postData) => {
     });
     const data = await res.json();
 	console.log(data);
-   window.location.href = "./posts.html"
+  if (data.errors) {
+		const error = document.querySelector("#error")
+		error.innerHTML = "you do no have permission to edit this post"
+	}
+	else {
+		window.location.href = "./posts.html"
+	}
+
 }
 
 };
